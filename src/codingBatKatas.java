f93f02dfeabf64f831135af8fe463b5ceb0afa33
thebellywolf <cdspenzer@gmail.com>
2023-08-23 20:36:44 -0400

    or35

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -203,7 +203,21 @@ public class codingBatKatas {
         return newString + str + newString;
     }
 
-    
+
+/*    Return true if the given non-negative number is a multiple of 3 or a multiple of 5. Use the % "mod" operator -- see Introduction to Mod
+    or35(3) → true
+    or35(10) → true
+    or35(8) → false*/
+
+    public boolean or35(int n) {
+
+        if ( n % 5 == 0 || n % 3 == 0) {
+
+            return true;
+        }
+        return false;
+
+    }
 
 
 
