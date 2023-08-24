c1ee8c990fa644a66417ca96c08bdc773221ea28
thebellywolf <cdspenzer@gmail.com>
2023-08-23 20:39:46 -0400

    startHi

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -242,7 +242,24 @@ public class codingBatKatas {
 
     }
 
+/*    Given a string, return true if the string starts with "hi" and false otherwise.
 
+    startHi("hi there") → true
+    startHi("hi") → true
+    startHi("hello hi") → false*/
+
+    public boolean startHi(String str) {
+
+
+        if (str.length() >= 2) {
+            if (str.substring(0,2).equals("hi")) {
+
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 
 
