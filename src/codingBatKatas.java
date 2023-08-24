fc77cabec3f6c45e21ad9e9c644b061a20ac8914
thebellywolf <cdspenzer@gmail.com>
2023-08-23 20:38:41 -0400

    front22

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -220,6 +220,39 @@ public class codingBatKatas {
     }
 
 
+   /* Given a string, take the first 2 chars and return the string with the 2 chars added at both the front and back, so "kitten" yields"kikittenki". If the string length is less than 2, use whatever chars are there.
+    front22("kitten") → "kikittenki"
+    front22("Ha") → "HaHaHa"
+    front22("abc") → "ababcab"*/
+
+    public String front22(String str) {
+
+        String frontEnd = "";
+
+        if (str.length() < 2 ) {
+
+            return str + str + str;
+
+        } else {
+
+            frontEnd = str.substring(0, 2);
+        }
+
+        return frontEnd + str + frontEnd;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
