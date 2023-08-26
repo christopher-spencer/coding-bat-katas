efedc6f6536744e3b5b70c1b36db97db24859a3b
thebellywolf <cdspenzer@gmail.com>
2023-08-25 21:49:25 -0400

    stringE

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -472,7 +472,24 @@ public class codingBatKatas {
     }
 
 
+/*    Return true if the given string contains between 1 and 3 'e' chars.
 
+    stringE("Hello") → true
+    stringE("Heelle") → true
+    stringE("Heelele") → false*/
+
+    public boolean stringE(String str) {
+
+        int counter = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if ( str.charAt(i) == 'e') {
+                counter += 1;
+            }
+        }
+
+        return counter >= 1 && counter <= 3;
+    }
 
 
 
