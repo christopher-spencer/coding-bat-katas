d3087869c0ec18cf2e6a5bccc5a205b2d1880bce
thebellywolf <cdspenzer@gmail.com>
2023-08-25 21:38:21 -0400

    max1020

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -451,4 +451,31 @@ public class codingBatKatas {
     }
 
 
+/*    Given 2 positive int values, return the larger value that is in the range 10..20 inclusive, or return 0 if neither is in that range.
+
+    max1020(11, 19) → 19
+    max1020(19, 11) → 19
+    max1020(11, 9) → 11*/
+
+    public int max1020(int a, int b) {
+
+
+        if ( (a >= 10 && a <= 20) && (b >= 10 && b <= 20) ) {
+            return Math.max(a,b);
+        } else if (a >= 10 && a <= 20) {
+            return a;
+        } else if (b >= 10 && b <= 20) {
+            return b;
+        }
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
 }
