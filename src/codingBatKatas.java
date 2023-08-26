b1e9d5b220e30f8de1b4113f74ba475a7fa9491c
thebellywolf <cdspenzer@gmail.com>
2023-08-26 14:17:08 -0400

    lastDigit

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -491,6 +491,15 @@ public class codingBatKatas {
         return counter >= 1 && counter <= 3;
     }
 
+/*    Given two non-negative int values, return true if they have the same last digit, such as with 27 and 57. Note that the % "mod" operator computes remainders, so 17 % 10 is 7.
+
+    lastDigit(7, 17) → true
+    lastDigit(6, 17) → false
+    lastDigit(3, 113) → true*/
+
+    public boolean lastDigit(int a, int b) {
+        return a % 10 == b % 10;
+    }
 
 
 
