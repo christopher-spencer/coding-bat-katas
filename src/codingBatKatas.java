28331796b4b931dd1d8457c532f35cf8b0e1a63b
thebellywolf <cdspenzer@gmail.com>
2023-09-19 15:25:02 -0400

    stringBits

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -622,6 +622,24 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+//    stringBits("Hello") → "Hlo"
+//    stringBits("Hi") → "H"
+//    stringBits("Heeololeo") → "Hello"
+
+
+    public String stringBits(String str) {
+
+        String newString = "";
+
+        for(int i = 0; i < str.length(); i+= 2 ) {
+            newString += str.substring(i, i+1);
+        }
+
+        return newString;
+
+    }
+
 
 
 
