f2df94f37014759afd2b07974c41bca877ee0a05
thebellywolf <cdspenzer@gmail.com>
2023-09-20 14:53:50 -0400

    stringSplosion

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -641,6 +641,24 @@ public class codingBatKatas {
     }
 
 
+//    Given a non-empty string like "Code" return a string like "CCoCodCode".
+//    stringSplosion("Code") → "CCoCodCode"
+//    stringSplosion("abc") → "aababc"
+//    stringSplosion("ab") → "aab"
+
+    public String stringSplosion(String str) {
+
+        String newString = "";
+
+        for(int i = 0; i < str.length(); i++) {
+            newString = newString + str.substring(0,i+1);
+        }
+
+        return newString;
+
+    }
+
+
 
 
 
