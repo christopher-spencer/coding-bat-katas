6705266fa4dd7015b5d865e60cd9532598ec9894
thebellywolf <cdspenzer@gmail.com>
2023-09-21 13:09:30 -0400

    firstHalf

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -984,6 +984,19 @@ public class codingBatKatas {
     }
 
 
+//    Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+//    firstHalf("WooHoo") → "Woo"
+//    firstHalf("HelloThere") → "Hello"
+//    firstHalf("abcdef") → "abc"
+
+    public String firstHalf(String str) {
+
+        int firstHalf = str.length() / 2;
+
+        return str.substring(0, firstHalf);
+
+    }
+
 
 
 
