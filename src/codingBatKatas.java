6d4a8a6ddc20360d244650e1fb348051e7ba5b7e
thebellywolf <cdspenzer@gmail.com>
2023-09-21 14:51:21 -0400

    withoutEnd2

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1091,6 +1091,22 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, return a version without both the first and last char of the string. The string may be any length, including 0.
+//    withouEnd2("Hello") → "ell"
+//    withouEnd2("abc") → "b"
+//    withouEnd2("ab") → ""
+
+    public String withouEnd2(String str) {
+
+        if (str.length() < 2 ) {
+            return "";
+        }
+
+        String middle = str.substring(1,str.length()-1);
+        return middle;
+    }
+
+
 
 
 
