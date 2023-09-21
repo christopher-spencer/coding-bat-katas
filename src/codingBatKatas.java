d5c63a9412f4235b90a35f0ade77f2916d86c38a
thebellywolf <cdspenzer@gmail.com>
2023-09-21 13:43:57 -0400

    nonStart

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1033,6 +1033,19 @@ public class codingBatKatas {
     }
 
 
+//    Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+//    nonStart("Hello", "There") → "ellohere"
+//    nonStart("java", "code") → "avaode"
+//    nonStart("shotl", "java") → "hotlava"
+
+    public String nonStart(String a, String b) {
+        String one = a.substring(1);
+        String two = b.substring(1);
+
+        return one + two;
+
+    }
+
 
 
 
