0563731e1803e91419adadda0eba710cc1f6875d
thebellywolf <cdspenzer@gmail.com>
2023-09-21 14:23:53 -0400

    lastTwo

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1058,8 +1058,22 @@ public class codingBatKatas {
         String newBeginnings = str.substring(2);
 
         return newBeginnings + newEnd;
+    }
+
+
+//    Given a string, return a "rotated right 2" version where the last 2 chars are moved to the start. The string length will be at least 2.
+//    right2("Hello") → "loHel"
+//    right2("java") → "vaja"
+//    right2("Hi") → "Hi"
+
+    public String right2(String str) {
+
+        String lastTwo = str.substring(str.length() - 2);
+
+        return lastTwo + str.substring(0,str.length() - 2);
 
     }
+
 
 
 
