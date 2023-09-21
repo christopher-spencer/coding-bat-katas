df737c1ed7e077ee69949b1d27675f99048f15ba
thebellywolf <cdspenzer@gmail.com>
2023-09-21 13:45:51 -0400

    eft2

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1047,6 +1047,23 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, return a "rotated left 2" version where the first 2 chars are moved to the end. The string length will be at least 2.
+//    left2("Hello") → "lloHe"
+//    left2("java") → "vaja"
+//    left2("Hi") → "Hi"
+
+    public String left2(String str) {
+
+        String newEnd = str.substring(0,2);
+        String newBeginnings = str.substring(2);
+
+        return newBeginnings + newEnd;
+
+    }
+
+
+
+
 
 
 }
