6bdca68dde4aa0a3e2d89c01cc7f184dd3add818
thebellywolf <cdspenzer@gmail.com>
2023-09-22 09:40:38 -0400

    endsLy

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1120,6 +1120,28 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, return true if it ends in "ly".
+//    endsLy("oddly") → true
+//    endsLy("y") → false
+//    endsLy("oddy") → false
+
+    public boolean endsLy(String str) {
+
+        if (str.length() < 2) {
+            return false;
+        }
+
+        if (str.substring(str.length() - 2).equals("ly") ) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    
+
+
 
 
 
