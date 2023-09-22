7d224ee1214866fe4c6745c7d815fd4adcd410ec
thebellywolf <cdspenzer@gmail.com>
2023-09-22 09:51:57 -0400

    nTwice

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1139,7 +1139,20 @@ public class codingBatKatas {
 
     }
 
-    
+//    Given a string and an int n, return a string made of the first and last n chars from the string. The string length will be at least n.
+//    nTwice("Hello", 2) → "Helo"
+//    nTwice("Chocolate", 3) → "Choate"
+//    nTwice("Chocolate", 1) → "Ce"
+
+    public String nTwice(String str, int n) {
+
+        String firstPart = str.substring(0,n);
+        String lastPart = str.substring(str.length() - n);
+
+        return firstPart + lastPart;
+
+    }
+
 
 
 
