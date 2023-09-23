90319b6423b78d6bca5f2e68379065b1fe3fd1ec
thebellywolf <cdspenzer@gmail.com>
2023-09-23 11:13:45 -0400

    conCat

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1243,8 +1243,31 @@ public class codingBatKatas {
         String lastB = b.substring(b.length() -1 );
 
         return firstA + lastB;
-
     }
+
+
+//    Given two strings, append them together (known as "concatenation") and return the result. However, if the concatenation creates a double-char, then omit one of the chars, so "abc" and "cat" yields "abcat".
+//    conCat("abc", "cat") → "abcat"
+//    conCat("dog", "cat") → "dogcat"
+//    conCat("abc", "") → "abc"
+
+    public String conCat(String a, String b) {
+
+        if (a.equals("") ) {
+            return b;
+        }
+
+        if (b.equals("") ) {
+            return a;
+        }
+
+        if (a.substring(a.length() -1).equals(b.substring(0,1)) ) {
+            return a + b.substring(1);
+        }
+
+        return a + b;
+    }
+
 
 
 
