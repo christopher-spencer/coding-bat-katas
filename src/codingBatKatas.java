a2657d008768396bfcf380ffc3e924c02d73355e
thebellywolf <cdspenzer@gmail.com>
2023-09-23 10:30:02 -0400

    atFirst

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1208,6 +1208,22 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, return a string length 2 made of its first 2 chars. If the string length is less than 2, use '@' for the missing chars.
+//    atFirst("hello") → "he"
+//    atFirst("hi") → "hi"
+//    atFirst("h") → "h@"
+
+    public String atFirst(String str) {
+
+        if (str.length() == 1) {
+            return str + "@";
+        } else if (str.length() == 0) {
+            return "@@";
+        }
+        return str.substring(0,2);
+    }
+
+    
 
 
 
