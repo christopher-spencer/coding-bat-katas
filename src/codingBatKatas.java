0002a49f42ce455cbf2b82a7fe9e2101c8ccc5ce
thebellywolf <cdspenzer@gmail.com>
2023-09-23 16:39:36 -0400

    deFront

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1377,6 +1377,26 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, return a version without the first 2 chars. Except keep the first char if it is 'a' and keep the second char if it is 'b'. The string may be any length. Harder than it looks.
+//    deFront("Hello") → "llo"
+//    deFront("java") → "va"
+//    deFront("away") → "aay"
+
+    public String deFront(String str) {
+
+        if ( str.substring(0,1).equals("a") && str.substring(1,2).equals("b") ) {
+            return "a" + "b" + str.substring(2);
+        } else if ( str.substring(0,1).equals("a") ) {
+            return "a" + str.substring(2);
+        } else if ( str.substring(1,2).equals("b") ) {
+            return "b" + str.substring(2);
+        } else {
+            return str.substring(2);
+        }
+
+    }
+
+
 
 
 
