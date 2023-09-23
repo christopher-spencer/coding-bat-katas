882084152fca073cf8bc703116f445db18fce7f7
thebellywolf <cdspenzer@gmail.com>
2023-09-23 11:34:08 -0400

    seeColor

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1287,6 +1287,27 @@ public class codingBatKatas {
     }
 
 
+//    Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+//    seeColor("redxx") → "red"
+//    seeColor("xxred") → ""
+//    seeColor("blueTimes") → "blue"
+
+    public String seeColor(String str) {
+
+        if (str.length() < 4 && !(str.equals("red")) ) {
+            return "";
+        } else if (str.substring(0,3).equals("red") ) {
+            return "red";
+        } else if (str.substring(0,4).equals("blue") ) {
+            return "blue";
+        } else {
+            return "";
+        }
+
+    }
+
+
+
 
 
 
