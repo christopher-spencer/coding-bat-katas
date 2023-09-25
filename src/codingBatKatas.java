29058f2d3155612dc18bc32701636ecf2af5ffea
thebellywolf <cdspenzer@gmail.com>
2023-09-25 16:39:22 -0400

    mapBully

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1,3 +1,5 @@
+import java.util.Map;
+
 public class codingBatKatas {
 
 /*    The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
@@ -1459,6 +1461,22 @@ public class codingBatKatas {
 
     }
 
+
+//    Modify and return the given map as follows: if the key "a" has a value, set the key "b" to have that value, and set the key "a" to have the value "". Basically "b" is a bully, taking the value and replacing it with the empty string.
+//    mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
+//    mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
+//    mapBully({"a": "candy", "b": "carrot", "c": "meh"}) → {"a": "", "b": "candy", "c": "meh"}
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+
+        if ( map.containsKey("a") ) {
+            map.put("b", map.get("a") );
+            map.put("a", "");
+        }
+
+        return map;
+
+    }
 
 
 
