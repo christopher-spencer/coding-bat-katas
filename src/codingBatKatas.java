f3b032a45f9a857af4f1bb45d819a1177f302a66
thebellywolf <cdspenzer@gmail.com>
2023-09-28 19:40:10 -0400

    midTriple

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1974,6 +1974,26 @@ public class codingBatKatas {
     }
 
 
+//    Given an array of ints of odd length, look at the first, last, and middle values in the array and return the largest. The array length will be a least 1.
+//    maxTriple([1, 2, 3]) → 3
+//    maxTriple([1, 5, 3]) → 5
+//    maxTriple([5, 2, 3]) → 5
+
+    public int maxTriple(int[] nums) {
+
+        if (nums.length < 2) {
+            return nums[0];
+        }
+
+        int middle = (nums.length/2);
+
+        int largest = Math.max( (Math.max(nums[0],nums[nums.length-1])),nums[middle] );
+
+        return largest;
+    }
+
+
+
 
 
 
