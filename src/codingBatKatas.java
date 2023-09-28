6db681e4050e3372a60108924baeddabd2806a8a
thebellywolf <cdspenzer@gmail.com>
2023-09-27 23:33:58 -0400

    makeEnd3

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1704,7 +1704,23 @@ public class codingBatKatas {
         return reverse;
     }
 
-    
+
+//    Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+//    maxEnd3([1, 2, 3]) → [3, 3, 3]
+//    maxEnd3([11, 5, 9]) → [11, 11, 11]
+//    maxEnd3([2, 11, 3]) → [3, 3, 3]
+
+    public int[] maxEnd3(int[] nums) {
+
+        int max = Math.max(nums[0],nums[nums.length-1]);
+        int[] changed = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            changed[i] = max;
+        }
+        return changed;
+    }
+
 
 
 
