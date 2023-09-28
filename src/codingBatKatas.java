c95a2210770c862cedd18588b8312b3dd65bba3f
thebellywolf <cdspenzer@gmail.com>
2023-09-28 19:37:26 -0400

    makeMiddle

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1905,6 +1905,23 @@ public class codingBatKatas {
         return a;
     }
 
+//    Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array. The original array will be length 2 or more.
+//    makeMiddle([1, 2, 3, 4]) → [2, 3]
+//    makeMiddle([7, 1, 2, 3, 4, 9]) → [2, 3]
+//    makeMiddle([1, 2]) → [1, 2]
+
+    public int[] makeMiddle(int[] nums) {
+        if (nums.length < 3) {
+            return nums;
+        }
+
+        int middle = (nums.length / 2);
+
+        int[] newArray = new int[] {nums[middle-1],nums[middle]};
+
+        return newArray;
+    }
+
 
 
 
