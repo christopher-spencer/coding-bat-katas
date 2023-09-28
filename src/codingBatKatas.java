7462d7c0870cf0fc43b69be075c288cdda3ac378
thebellywolf <cdspenzer@gmail.com>
2023-09-28 19:38:06 -0400

    plusTwo

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1923,6 +1923,19 @@ public class codingBatKatas {
     }
 
 
+//    Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+//    plusTwo([1, 2], [3, 4]) → [1, 2, 3, 4]
+//    plusTwo([4, 4], [2, 2]) → [4, 4, 2, 2]
+//    plusTwo([9, 2], [3, 4]) → [9, 2, 3, 4]
+
+    public int[] plusTwo(int[] a, int[] b) {
+
+        int[] bigGuy = new int[] {a[0],a[1],b[0],b[1]};
+
+        return bigGuy;
+    }
+
+
 
 
 
