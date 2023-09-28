12e45f828e9bab618184f2ec64499da0fb26394c
thebellywolf <cdspenzer@gmail.com>
2023-09-27 23:30:10 -0400

    makePi

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1643,6 +1643,17 @@ public class codingBatKatas {
     }
 
 
+//    Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+//    makePi() → [3, 1, 4]
+
+    public int[] makePi() {
+
+        int[] pi = new int[] {3,1,4};
+
+        return pi;
+
+    }
+
 
 
 
