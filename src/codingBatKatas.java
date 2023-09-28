9436672d047e395fd4d52accd4071950bbdb85cf
thebellywolf <cdspenzer@gmail.com>
2023-09-28 19:39:34 -0400

    midThree

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1955,6 +1955,24 @@ public class codingBatKatas {
     }
 
 
+//    Given an array of ints of odd length, return a new array length 3 containing the elements from the middle of the array. The array length will be at least 3.
+//    midThree([1, 2, 3, 4, 5]) → [2, 3, 4]
+//    midThree([8, 6, 7, 5, 3, 0, 9]) → [7, 5, 3]
+//    midThree([1, 2, 3]) → [1, 2, 3]
+
+    public int[] midThree(int[] nums) {
+
+        if (nums.length == 3) {
+            return nums;
+        }
+
+        int middle = nums.length/2;
+
+        int[] newArray = new int[] {nums[middle-1],nums[middle],nums[middle+1]};
+
+        return newArray;
+    }
+
 
 
 
