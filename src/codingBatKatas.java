3e651c501fc60035242ade28330e041441204bb7
thebellywolf <cdspenzer@gmail.com>
2023-09-28 19:36:41 -0400

    biggerTwo

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1874,6 +1874,40 @@ public class codingBatKatas {
     }
 
 
+//    Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array. Return the array which has the largest sum. In event of a tie, return a.
+//    biggerTwo([1, 2], [3, 4]) → [3, 4]
+//    biggerTwo([3, 4], [1, 2]) → [3, 4]
+//    biggerTwo([1, 1], [1, 2]) → [1, 2]
+
+    public int[] biggerTwo(int[] a, int[] b) {
+        int sumA = 0;
+        int sumB = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sumA+= a[i];
+        }
+
+        for (int j = 0; j < b.length; j++) {
+            sumB+= b[j];
+        }
+
+        if (sumA == sumB) {
+            return a;
+        }
+
+        int largest = Math.max(sumA,sumB);
+
+        if (largest == sumA) {
+            return a;
+        } else if (largest == sumB) {
+            return b;
+        }
+        return a;
+    }
+
+
+
+
 
 
 
