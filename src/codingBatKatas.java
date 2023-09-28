7f528ba3e662955dae44221be751b0e7c3761601
thebellywolf <cdspenzer@gmail.com>
2023-09-28 19:29:25 -0400

    sum2fix

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -1727,7 +1727,15 @@ public class codingBatKatas {
 //    sum2([1, 1]) → 2
 //    sum2([1, 1, 1, 1]) → 2
 
-    
+    public int sum2(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        } else if (nums.length == 1) {
+            return nums[0];
+        } else {
+            return nums[0] + nums[1];
+        }
+    }
 
 
 
