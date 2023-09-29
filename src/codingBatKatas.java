0f7715b700bb884e85940ca3d84a2dac8cbac681
thebellywolf <cdspenzer@gmail.com>
2023-09-29 16:18:06 -0400

    firstChar

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2172,6 +2172,31 @@ public class codingBatKatas {
     }
 
 
+//    Given an array of non-empty strings, return a Map<String, String> with a key for every different first character seen, with the value of all the strings starting with that character appended together in the order they appear in the array.
+//    firstChar(["salt", "tea", "soda", "toast"]) → {"s": "saltsoda", "t": "teatoast"}
+//    firstChar(["aa", "bb", "cc", "aAA", "cCC", "d"]) → {"a": "aaaAA", "b": "bb", "c": "cccCC", "d": "d"}
+//    firstChar([]) → {}
+
+    public Map<String, String> firstChar(String[] strings) {
+
+        Map<String,String> map = new HashMap<String,String>();
+
+        for(String word : strings) {
+            String firstLetter = word.substring(0,1);
+
+            if(map.containsKey(firstLetter)) {
+                String previousWord = map.get(firstLetter);
+                map.put(firstLetter,previousWord+word);
+            } else {
+                map.put(firstLetter,word);
+            }
+        }
+
+        return map;
+    }
+
+
+
 
 
 
