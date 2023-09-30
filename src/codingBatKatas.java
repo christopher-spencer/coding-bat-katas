61708101c61509317254574c4ee7093c55bd5d5c
thebellywolf <cdspenzer@gmail.com>
2023-09-30 12:50:45 -0400

    wordMultiple

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2225,6 +2225,25 @@ public class codingBatKatas {
     }
 
 
+//    Given an array of strings, return a Map<String, Boolean> where each different string is a key and its value is true if that string appears 2 or more times in the array.
+//    wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}
+//    wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}
+//    wordMultiple(["c", "c", "c", "c"]) → {"c": true}
+
+    public Map<String, Boolean> wordMultiple(String[] strings) {
+
+        Map<String,Boolean> map = new HashMap<String,Boolean>();
+
+        for (String string : strings) {
+            if (map.containsKey(string)) {
+                map.put(string, true);
+            } else {
+                map.put(string, false);
+            }
+        }
+        return map;
+    }
+
 
 
 
