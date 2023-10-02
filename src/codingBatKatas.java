faea85a41ad8b517ee35734ca515bc61941c1372
thebellywolf <cdspenzer@gmail.com>
2023-10-02 10:32:01 -0400

    firstSwap

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2197,7 +2197,37 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         return strings;
     }
 
+//    We'll say that 2 strings "match" if they are non-empty and their first chars are the same. Loop over and then return
+//    the given array of non-empty strings as follows: if a string matches an earlier string in the array, swap the 2
+//    strings in the array. A particular first char can only cause 1 swap, so once a char has caused a swap, its later
+//    swaps are disabled. Using a map, this can be solved making just one pass over the array. More difficult than it looks.
+//    firstSwap(["ab", "ac"]) → ["ac", "ab"]
+//    firstSwap(["ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"]) → ["ay", "by", "cy", "cx", "bx", "ax", "aaa", "azz"]
+//    firstSwap(["ax", "bx", "ay", "by", "ai", "aj", "bx", "by"]) → ["ay", "by", "ax", "bx", "ai", "aj", "bx", "by"]
 
+    public String[] firstSwap(String[] strings) {
+        Map<String, Integer> firstCharMap = new HashMap<>();
+
+        for (int i = 0; i < strings.length; i++) {
+            String currentString = strings[i];
+            String firstChar = currentString.substring(0,1);
+
+            if (firstCharMap.containsKey(firstChar)) {
+                int swapIndex = firstCharMap.get(firstChar);
+
+                if (swapIndex != -1) {
+                    String temp = strings[swapIndex];
+                    strings[swapIndex] = currentString;
+                    strings[i] = temp;
+
+                    firstCharMap.put(firstChar, -1);
+                }
+            } else {
+                firstCharMap.put(firstChar, i);
+            }
+        }
+        return strings;
+    }
 
 
 
