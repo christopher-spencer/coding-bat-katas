69d45e33d9959500cbbea02759981def02c88c9b
thebellywolf <cdspenzer@gmail.com>
2023-10-02 23:37:12 -0400

    teenSum

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2421,7 +2421,21 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return (num % 10 <= 2 || num % 10 >= 8);
     }
 
-    
+//    Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. So if either value is a teen, just return 19.
+//    teenSum(3, 4) → 7
+//    teenSum(10, 13) → 19
+//    teenSum(13, 2) → 19
+
+    public int teenSum(int a, int b) {
+        int sum = a + b;
+
+        if ( (a >= 13 && a <=19) || (b >= 13 && b <=19) ) {
+            return 19;
+        } else {
+            return sum;
+        }
+    }
+
 
 
 
