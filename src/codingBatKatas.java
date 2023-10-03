21d743ec1f27b536e6f641ae42ccc190c9fe1e61
thebellywolf <cdspenzer@gmail.com>
2023-10-02 20:03:18 -0400

    squirrelPlay

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2242,7 +2242,6 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         }
     }
 
-
 //    You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
 //    dateFashion(5, 10) → 2
 //    dateFashion(5, 2) → 0
@@ -2261,6 +2260,22 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
             return maybeYouGetATable;
         }
     }
+
+//    The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean isSummer, return true if the squirrels play and false otherwise.
+//    squirrelPlay(70, false) → true
+//    squirrelPlay(95, false) → false
+//    squirrelPlay(95, true) → true
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+
+        if (temp >= 60 && temp <= 90 && !isSummer) {
+            return true;
+        } else if (temp >= 60 && temp <= 100 && isSummer) {
+            return true;
+        }
+        return false;
+    }
+
 
 
 
