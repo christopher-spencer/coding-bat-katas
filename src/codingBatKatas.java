15667aa6f58b4869c5f24a1e33d886e97575bec0
thebellywolf <cdspenzer@gmail.com>
2023-10-03 12:51:47 -0400

    teaParty

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2229,7 +2229,9 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         return strings;
     }
 
-//    When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return true if the party with the given values is successful, or false otherwise.
+//    When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number
+//    of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the
+//    number of cigars. Return true if the party with the given values is successful, or false otherwise.
 //    cigarParty(30, false) → false
 //    cigarParty(50, false) → true
 //    cigarParty(70, true) → true
@@ -2242,7 +2244,11 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         }
     }
 
-//    You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
+//    You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your
+//    clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table
+//    is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result
+//    is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise
+//    the result is 1 (maybe).
 //    dateFashion(5, 10) → 2
 //    dateFashion(5, 2) → 0
 //    dateFashion(5, 5) → 1
@@ -2261,7 +2267,9 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         }
     }
 
-//    The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean isSummer, return true if the squirrels play and false otherwise.
+//    The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between
+//    60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature
+//    and a boolean isSummer, return true if the squirrels play and false otherwise.
 //    squirrelPlay(70, false) → true
 //    squirrelPlay(95, false) → false
 //    squirrelPlay(95, true) → true
@@ -2276,7 +2284,10 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         return false;
     }
 
-//    You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+//    You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an
+//    int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between
+//    61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on
+//    that day, your speed can be 5 higher in all cases.
 //    caughtSpeeding(60, false) → 0
 //    caughtSpeeding(65, false) → 1
 //    caughtSpeeding(65, true) → 0
@@ -2296,7 +2307,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
     return noTicket;
 }
 
-//    Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+//    Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in
+//    that case just return 20.
 //    sortaSum(3, 4) → 7
 //    sortaSum(9, 4) → 20
 //    sortaSum(10, 11) → 21
@@ -2312,7 +2324,10 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
-//    Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+//    Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation,
+//    return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00"
+//    and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and
+//    weekends it should be "off".
 //    alarmClock(1, false) → "7:00"
 //    alarmClock(5, false) → "7:00"
 //    alarmClock(0, false) → "10:00"
@@ -2337,7 +2352,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
-//    The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
+//    The number 6 is a truly great number. Given two int values, a and b, return true if either one is 6. Or if their
+//    sum or difference is 6. Note: the function Math.abs(num) computes the absolute value of a number.
 //    love6(6, 4) → true
 //    love6(4, 5) → false
 //    love6(1, 5) → true
@@ -2353,7 +2369,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
-//    Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in which case return true if the number is less or equal to 1, or greater or equal to 10.
+//    Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in which case
+//    return true if the number is less or equal to 1, or greater or equal to 10.
 //    in1To10(5, false) → true
 //    in1To10(11, false) → false
 //    in1To10(11, true) → true
@@ -2370,7 +2387,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return false;
     }
 
-//    We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. Return true if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
+//    We'll say a number is special if it is a multiple of 11 or if it is one more than a multiple of 11. Return true
+//    if the given non-negative number is special. Use the % "mod" operator -- see Introduction to Mod
 //    specialEleven(22) → true
 //    specialEleven(23) → true
 //    specialEleven(24) → false
@@ -2388,7 +2406,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return n % 20 == 1 || n % 20 == 2;
     }
 
-//    Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator -- see Introduction to Mod
+//    Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator
+//    -- see Introduction to Mod
 //    old35(3) → true
 //    old35(10) → true
 //    old35(15) → false
@@ -2403,7 +2422,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
-//    Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return true, but 40 returns false. See also: Introduction to Mod
+//    Return true if the given non-negative number is 1 or 2 less than a multiple of 20. So for example 38 and 39 return
+//    true, but 40 returns false. See also: Introduction to Mod
 //    less20(18) → true
 //    less20(19) → true
 //    less20(20) → false
@@ -2412,7 +2432,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return n % 20 == 19 || n % 20 == 18;
     }
 
-//    Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
+//    Given a non-negative number "num", return true if num is within 2 of a multiple of 10. Note: (a % b) is the
+//    remainder of dividing a by b, so (7 % 5) is 2. See also: Introduction to Mod
 //    nearTen(12) → true
 //    nearTen(17) → false
 //    nearTen(19) → true
@@ -2421,7 +2442,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return (num % 10 <= 2 || num % 10 >= 8);
     }
 
-//    Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky. So if either value is a teen, just return 19.
+//    Given 2 ints, a and b, return their sum. However, "teen" values in the range 13..19 inclusive, are extra lucky.
+//    So if either value is a teen, just return 19.
 //    teenSum(3, 4) → 7
 //    teenSum(10, 13) → 19
 //    teenSum(13, 2) → 19
@@ -2436,7 +2458,8 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
-//    Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you only answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
+//    Your cell phone rings. Return true if you should answer it. Normally you answer, except in the morning you only
+//    answer if it is your mom calling. In all cases, if you are asleep, you do not answer.
 //    answerCell(false, false, false) → true
 //    answerCell(false, false, true) → false
 //    answerCell(true, false, false) → false
@@ -2448,6 +2471,28 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
             return false;
         } else {
             return true;
+        }
+    }
+
+//    We are having a party with amounts of tea and candy. Return the int outcome of the party encoded as 0=bad, 1=good,
+//    or 2=great. A party is good (1) if both tea and candy are at least 5. However, if either tea or candy is at least
+//    double the amount of the other one, the party is great (2). However, in all cases, if either tea or candy is less
+//    than 5, the party is always bad (0).
+//    teaParty(6, 8) → 1
+//    teaParty(3, 8) → 0
+//    teaParty(20, 6) → 2
+
+    public int teaParty(int tea, int candy) {
+        int bad = 0;
+        int good = 1;
+        int great = 2;
+
+        if (tea < 5 || candy < 5) {
+            return bad;
+        } else if ( (tea >= candy * 2) || (candy >= tea * 2) ) {
+            return great;
+        } else {
+            return good;
         }
     }
 
