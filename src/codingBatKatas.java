95bf96d12f8de853b0ea6cbea6153ae9bded6c83
thebellywolf <cdspenzer@gmail.com>
2023-10-03 13:04:04 -0400

    shareDigit

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2709,6 +2709,20 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
+//    Given two ints, each in the range 10..99, return true if there is a digit that appears in both numbers, such as the
+//    2 in 12 and 23. (Note: division, e.g. n/10, gives the left digit while the % "mod" n%10 gives the right digit.)
+//    shareDigit(12, 23) → true
+//    shareDigit(12, 43) → false
+//    shareDigit(12, 44) → false
+
+    public boolean shareDigit(int a, int b) {
+        int aRight = a % 10;
+        int bRight = b % 10;
+        int aLeft = a / 10;
+        int bLeft = b / 10;
+
+        return (aRight == bRight || aRight == bLeft || aLeft == bRight || aLeft == bLeft);
+    }
 
 
 
