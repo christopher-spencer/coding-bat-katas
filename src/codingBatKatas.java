499cf407dbdd3b2c64bc77c80212334c2d5dffff
thebellywolf <cdspenzer@gmail.com>
2023-10-02 23:32:57 -0400

    in1To10

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2353,6 +2353,22 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         }
     }
 
+//    Given a number n, return true if n is in the range 1..10, inclusive. Unless outsideMode is true, in which case return true if the number is less or equal to 1, or greater or equal to 10.
+//    in1To10(5, false) → true
+//    in1To10(11, false) → false
+//    in1To10(11, true) → true
+
+    public boolean in1To10(int n, boolean outsideMode) {
+
+        if (!outsideMode) {
+            return n >= 1 && n <= 10;
+        } else if (outsideMode) {
+            if (n <= 1 || n >= 10) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
