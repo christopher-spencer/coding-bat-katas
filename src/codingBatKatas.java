70e3fcdb77162bfb365f58f7f58561391d055048
thebellywolf <cdspenzer@gmail.com>
2023-10-02 23:34:28 -0400

    more20

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2379,7 +2379,15 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return n % 11 == 0 || n % 11 == 1;
     }
 
-    
+//    Return true if the given non-negative number is 1 or 2 more than a multiple of 20. See also: Introduction to Mod
+//    more20(20) → false
+//    more20(21) → true
+//    more20(22) → true
+
+    public boolean more20(int n) {
+        return n % 20 == 1 || n % 20 == 2;
+    }
+
 
 
 
