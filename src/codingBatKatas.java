6c9b99f4a03f597305be541f6c81bdc42877c534
thebellywolf <cdspenzer@gmail.com>
2023-10-02 20:04:09 -0400

    caughtSpeeding

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2276,6 +2276,30 @@ so "cat" yields "tcatt". The original string will be length 1 or more.
         return false;
     }
 
+//    You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+//    caughtSpeeding(60, false) → 0
+//    caughtSpeeding(65, false) → 1
+//    caughtSpeeding(65, true) → 0
+
+public int caughtSpeeding(int speed, boolean isBirthday) {
+    int noTicket = 0;
+    int smallTicket = 1;
+    int bigTicket = 2;
+
+    if (speed <= 60 || (speed <= 65 && isBirthday) ) {
+        return noTicket;
+    } else if ((speed >= 61 && speed <= 80) || (speed >= 66 && speed <= 85 && isBirthday) ) {
+        return smallTicket;
+    } else if (speed >= 81 || (speed >= 86 && isBirthday) ) {
+        return bigTicket;
+    }
+    return noTicket;
+}
+
+
+
+
+
 
 
 
