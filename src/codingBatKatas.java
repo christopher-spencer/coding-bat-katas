ba2efa80b3158ce985ce83a059678e37b1bd3abf
thebellywolf <cdspenzer@gmail.com>
2023-10-03 13:04:54 -0400

    sumLimit

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2724,6 +2724,25 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
         return (aRight == bRight || aRight == bLeft || aLeft == bRight || aLeft == bLeft);
     }
 
+//    Given 2 non-negative ints, a and b, return their sum, so long as the sum has the same number of digits as a. If the
+//    sum has more digits than a, just return a without b. (Note: one way to compute the number of digits of a non-negative
+//    int n is to convert it to a string with String.valueOf(n) and then check the length of the string.)
+//    sumLimit(2, 3) → 5
+//    sumLimit(8, 3) → 8
+//    sumLimit(8, 1) → 9
+
+    public int sumLimit(int a, int b) {
+        int sum = a + b;
+        String aString = Integer.toString(a);
+        String sumString = Integer.toString(sum);
+
+        if (sumString.length() == aString.length()) {
+            return sum;
+        } else {
+            return a;
+        }
+    }
+
 
 
 
