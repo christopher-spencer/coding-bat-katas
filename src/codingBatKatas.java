234a233675ad1ded6d0c7a603359938169eaa352
thebellywolf <cdspenzer@gmail.com>
2023-10-02 20:04:59 -0400

    sortaSum

--- src/codingBatKatas.java
+++ src/codingBatKatas.java
@@ -2296,6 +2296,22 @@ public int caughtSpeeding(int speed, boolean isBirthday) {
     return noTicket;
 }
 
+//    Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+//    sortaSum(3, 4) → 7
+//    sortaSum(9, 4) → 20
+//    sortaSum(10, 11) → 21
+
+    public int sortaSum(int a, int b) {
+
+        int sum = a + b;
+
+        if (sum < 10 || sum > 19) {
+            return sum;
+        } else {
+            return 20;
+        }
+    }
+
 
 
 
