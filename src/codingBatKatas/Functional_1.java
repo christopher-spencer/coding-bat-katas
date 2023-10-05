3cb11a8a5b580707a2bb599c9db5f9fcef0184a4
thebellywolf <cdspenzer@gmail.com>
2023-10-05 15:39:32 -0400

    lower

--- src/codingBatKatas/Functional_1.java
+++ src/codingBatKatas/Functional_1.java
@@ -75,6 +75,17 @@ public class Functional_1 {
         return nums;
     }
 
+//    Given a list of strings, return a list where each string is converted to lower case (Note: String toLowerCase() method).
+//    lower(["Hello", "Hi"]) → ["hello", "hi"]
+//    lower(["AAA", "BBB", "ccc"]) → ["aaa", "bbb", "ccc"]
+//    lower(["KitteN", "ChocolaTE"]) → ["kitten", "chocolate"]
+
+    public List<String> lower(List<String> strings) {
+        strings.replaceAll(s -> s.toLowerCase() );
+        return strings;
+    }
+
+
 
 
 
