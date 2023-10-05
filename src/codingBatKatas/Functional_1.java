07509835d783f3c3420296447564aa5a333511b4
thebellywolf <cdspenzer@gmail.com>
2023-10-05 15:35:59 -0400

    addStar

--- src/codingBatKatas/Functional_1.java
+++ src/codingBatKatas/Functional_1.java
@@ -25,6 +25,16 @@ public class Functional_1 {
         return nums;
     }
 
+//    Given a list of strings, return a list where each string has "*" added at its end.
+//    addStar(["a", "bb", "ccc"]) → ["a*", "bb*", "ccc*"]
+//    addStar(["hello", "there"]) → ["hello*", "there*"]
+//    addStar(["*"]) → ["**"]
+
+    public List<String> addStar(List<String> strings) {
+        strings.replaceAll(s -> s+= "*");
+        return strings;
+    }
+
 
 
 
