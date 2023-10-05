28b8c14aacfeece6d41dd610a65888acc86c0aa3
thebellywolf <cdspenzer@gmail.com>
2023-10-05 15:26:32 -0400

    blackjack

--- src/codingBatKatas/Logic_2.java
+++ src/codingBatKatas/Logic_2.java
@@ -126,6 +126,24 @@ public class Logic_2 {
         }
     }
 
+//    Given 2 int values greater than 0, return whichever value is nearest to 21 without going over. Return 0 if
+//    they both go over.
+//    blackjack(19, 21) → 21
+//    blackjack(21, 19) → 21
+//    blackjack(19, 22) → 19
+
+    public int blackjack(int a, int b) {
+        int largest = Math.max(a,b);
+        int smallest = Math.min(a,b);
+
+        if (largest <= 21) {
+            return largest;
+        } else if (largest >= 21 && smallest <= 21) {
+            return smallest;
+        } else {
+            return 0;
+        }
+    }
 
 
 
