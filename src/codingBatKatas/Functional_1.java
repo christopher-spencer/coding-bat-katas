50f81b7fe761b24e8c70fb50a15ce70fed079eab
thebellywolf <cdspenzer@gmail.com>
2023-10-05 15:30:21 -0400

    doubling

--- src/codingBatKatas/Functional_1.java
+++ src/codingBatKatas/Functional_1.java
@@ -1,4 +1,27 @@
 package codingBatKatas;
 
+import java.util.List;
+
 public class Functional_1 {
+
+
+//    Given a list of integers, return a list where each integer is multiplied by 2.
+//    doubling([1, 2, 3]) → [2, 4, 6]
+//    doubling([6, 8, 6, 8, -1]) → [12, 16, 12, 16, -2]
+//    doubling([]) → []
+
+    public List<Integer> doubling(List<Integer> nums) {
+        nums.replaceAll(n -> n * 2);
+        return nums;
+    }
+
+
+
+
+
+
+
+
+
+
 }
