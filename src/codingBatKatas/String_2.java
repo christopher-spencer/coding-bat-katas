5e811a5550970b69c7b20622ce9ec296458f5890
thebellywolf <cdspenzer@gmail.com>
2023-10-06 19:46:02 -0400

    doubleChar

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -2,7 +2,21 @@ package codingBatKatas;
 
 public class String_2 {
 
-    
+//    Given a string, return a string where for every char in the original, there are two chars.
+//    doubleChar("The") → "TThhee"
+//    doubleChar("AAbb") → "AAAAbbbb"
+//    doubleChar("Hi-There") → "HHii--TThheerree"
+
+    public String doubleChar(String str) {
+        String newString = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            newString += str.substring(i,i+1) + str.substring(i,i+1);
+        }
+
+        return newString;
+    }
+
 
 
 
