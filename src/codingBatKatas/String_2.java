a387d3e3caa28828c3f43a785c4172fe94985cd1
thebellywolf <cdspenzer@gmail.com>
2023-10-06 19:47:31 -0400

    catDog

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -33,6 +33,26 @@ public class String_2 {
         return counter;
     }
 
+//    Return true if the string "cat" and "dog" appear the same number of times in the given string.
+//    catDog("catdog") → true
+//    catDog("catcat") → false
+//    catDog("1cat1cadodog") → true
+
+    public boolean catDog(String str) {
+        int catCounter = 0;
+        int dogCounter = 0;
+
+        for (int i = 0; i < str.length()-2;i++) {
+            if ( str.substring(i,i+3).equals("dog") ) {
+                dogCounter++;
+            }
+            if ( str.substring(i,i+3).equals("cat") ) {
+                catCounter++;
+            }
+        }
+        return catCounter == dogCounter;
+    }
+
 
 
 
