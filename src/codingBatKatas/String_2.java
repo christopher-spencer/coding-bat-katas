06a1ebc1679d146f387a31899d4ba429a725c442
thebellywolf <cdspenzer@gmail.com>
2023-10-06 19:49:27 -0400

    endOther

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -70,6 +70,26 @@ public class String_2 {
         return counter;
     }
 
+//    Given two strings, return true if either of the strings appears at the very end of the other string,
+//    ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+//    Note: str.toLowerCase() returns the lowercase version of a string.
+//    endOther("Hiabc", "abc") → true
+//    endOther("AbC", "HiaBc") → true
+//    endOther("abc", "abXabc") → true
+
+    public boolean endOther(String a, String b) {
+
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        if (a.endsWith(b) || b.endsWith(a)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
 
 
 
