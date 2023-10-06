7474abef921c0b6a4e44cb03fa1b7566847ae57f
thebellywolf <cdspenzer@gmail.com>
2023-10-06 19:50:17 -0400

    xyzThere

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -89,6 +89,29 @@ public class String_2 {
         return false;
     }
 
+//    Return true if the given string contains an appearance of "xyz" where the xyz is not directly preceeded
+//    by a period (.). So "xxyz" counts but "x.xyz" does not.
+//    xyzThere("abcxyz") → true
+//    xyzThere("abc.xyz") → false
+//    xyzThere("xyz.abc") → true
+
+    public boolean xyzThere(String str) {
+
+        if (str.length() >= 3) {
+            if (str.substring(0,3).equals("xyz") ) {
+                return true;
+            }
+        }
+
+        for (int i = 1; i < str.length()-3;i++) {
+            if ( !(str.substring(i,i+1).equals(".")) && str.substring(i+1,i+4).equals("xyz")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 
 
