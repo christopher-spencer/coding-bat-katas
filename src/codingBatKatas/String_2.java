5c4b17b131011c770cc1c583fa8f2b3cb1c0ede1
thebellywolf <cdspenzer@gmail.com>
2023-10-07 10:53:39 -0400

    xyzMiddle

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -243,6 +243,26 @@ public class String_2 {
         return isTwice;
     }
 
+//    Given a string, does "xyz" appear in the middle of the string? To define middle, we'll say that the number of chars
+//    to the left and right of the "xyz" must differ by at most one. This problem is harder than it looks.
+//    xyzMiddle("AAxyzBB") → true
+//    xyzMiddle("AxyzBB") → true
+//    xyzMiddle("AxyzBBB") → false
+
+    public boolean xyzMiddle(String str) {
+        int middle = str.length() / 2;
+
+        if (str.length() < 3) {
+            return false;
+        }
+
+        if (str.length() % 2 == 0) {
+            return str.substring(middle-2,middle+1).equals("xyz") ||
+                    str.substring(middle-1,middle+2).equals("xyz");
+        } else {
+            return str.substring(middle-1,middle+2).equals("xyz");
+        }
+    }
 
 
 
