cf750379efefa65e19cf6eca6abf894a3db49c9d
thebellywolf <cdspenzer@gmail.com>
2023-10-07 10:57:25 -0400

    starOut

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -336,7 +336,36 @@ public class String_2 {
         return newString;
     }
 
+//    Return a version of the given string, where for every star (*) in the string the star and the chars immediately to
+//    its left and right are gone. So "ab*cd" yields "ad" and "ab**cd" also yields "ad".
+//    starOut("ab*cd") → "ad"
+//    starOut("ab**cd") → "ad"
+//    starOut("sm*eilly") → "silly"
 
+    public String starOut(String str) {
+        String newString = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '*') {
+                continue;
+            }
+
+            boolean skip = false;
+
+            if (i > 0 && str.charAt(i - 1) == '*') {
+                skip = true;
+            }
+
+            if (i < str.length() - 1 && str.charAt(i + 1) == '*') {
+                skip = true;
+            }
+
+            if (!skip) {
+                newString += str.charAt(i);
+            }
+        }
+        return newString;
+    }
 
 
 
