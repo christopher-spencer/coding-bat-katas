e689afb0d90a8877460375091a679dcc3c9e91d4
thebellywolf <cdspenzer@gmail.com>
2023-10-07 10:52:14 -0400

    repeatSeperator

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -208,6 +208,27 @@ public class String_2 {
         return newString;
     }
 
+//    Given two strings, word and a separator sep, return a big string made of count occurrences of the word, separated
+//    by the separator string.
+//    repeatSeparator("Word", "X", 3) → "WordXWordXWord"
+//    repeatSeparator("This", "And", 2) → "ThisAndThis"
+//    repeatSeparator("This", "And", 1) → "This"
+
+    public String repeatSeparator(String word, String sep, int count) {
+        String newString = "";
+        int countDown = count;
+
+        for (int i = 0; i < count; i ++) {
+            newString += word;
+            countDown--;
+            if (countDown > 0) {
+                newString += sep;
+            }
+        }
+        return newString;
+    }
+
+
 
 
 
