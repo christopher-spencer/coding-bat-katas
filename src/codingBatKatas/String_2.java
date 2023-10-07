a91b47569b05792afef0ccc8ca7272e24499e294
thebellywolf <cdspenzer@gmail.com>
2023-10-07 10:58:06 -0400

    plusOut

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -367,7 +367,26 @@ public class String_2 {
         return newString;
     }
 
+//    Given a string and a non-empty word string, return a version of the original String where all chars have been
+//    replaced by pluses ("+"), except for appearances of the word string which are preserved unchanged.
+//    plusOut("12xy34", "xy") → "++xy++"
+//    plusOut("12xy34", "1") → "1+++++"
+//    plusOut("12xy34xyabcxy", "xy") → "++xy++xy+++xy"
 
+    public String plusOut(String str, String word) {
+        String newString = "";
+        int wordLength = word.length();
+
+        for (int i = 0; i < str.length();i++) {
+            if (i + wordLength <= str.length() && str.substring(i, i + wordLength).equals(word)) {
+                newString += word;
+                i += wordLength-1;
+            } else {
+                newString += "+";
+            }
+        }
+        return newString;
+    }
 
 
 
