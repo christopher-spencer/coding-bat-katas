e34bf188944a288e8cda10eeee7b7822b1c14c81
thebellywolf <cdspenzer@gmail.com>
2023-10-07 10:54:24 -0400

    getSandwich

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -264,6 +264,22 @@ public class String_2 {
         }
     }
 
+//    A sandwich is two pieces of bread with something in between. Return the string that is between the first and last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
+//    getSandwich("breadjambread") → "jam"
+//    getSandwich("xxbreadjambreadyy") → "jam"
+//    getSandwich("xxbreadyy") → ""
+
+    public String getSandwich(String str) {
+        int firstBreadIndex = str.indexOf("bread");
+        int lastBreadIndex = str.lastIndexOf("bread");
+
+        if (firstBreadIndex != -1 && lastBreadIndex != -1 && firstBreadIndex != lastBreadIndex) {
+            return str.substring(firstBreadIndex + 5, lastBreadIndex);
+        } else {
+            return "";
+        }
+    }
+
 
 
 
