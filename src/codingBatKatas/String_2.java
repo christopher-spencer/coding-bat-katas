8eb6f13d72b391da78ca8719701d47dbd0463d6c
thebellywolf <cdspenzer@gmail.com>
2023-10-07 10:58:54 -0400

    wordEnds

--- src/codingBatKatas/String_2.java
+++ src/codingBatKatas/String_2.java
@@ -388,10 +388,26 @@ public class String_2 {
         return newString;
     }
 
+//    Given a string and a non-empty word string, return a string made of each char just before and just after every
+//    appearance of the word in the string. Ignore cases where there is no char before or after the word, and a char may
+//    be included twice if it is between two words.
+//    wordEnds("abcXY123XYijk", "XY") → "c13i"
+//    wordEnds("XY123XY", "XY") → "13"
+//    wordEnds("XY1XY", "XY") → "11"
 
+    public String wordEnds(String str, String word) {
+        String newString = "";
 
-
-
-
-
+        for (int i = 0; i < str.length(); i++) {
+            if (i + word.length() <= str.length() && str.substring(i, i + word.length()).equals(word)) {
+                if (i > 0) {
+                    newString += str.charAt(i - 1);
+                }
+                if (i + word.length() < str.length()) {
+                    newString += str.charAt(i + word.length());
+                }
+            }
+        }
+        return newString;
+    }
 }
