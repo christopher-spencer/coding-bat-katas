62e6c9e7896ad4a6227bf35be1b804830d1e14eb
thebellywolf <cdspenzer@gmail.com>
2023-10-13 21:54:07 -0400

    maxBlock

--- src/codingBatKatas/String_3.java
+++ src/codingBatKatas/String_3.java
@@ -206,6 +206,35 @@ public int countYZ(String str) {
         return newString;
     }
 
+//    Given a string, return the length of the largest "block" in the string. A block is a run of adjacent chars that are the same.
+//    maxBlock("hoopla") → 2
+//    maxBlock("abbCCCddBBBxx") → 3
+//    maxBlock("") → 0
+
+    public int maxBlock(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        int largest = 0;
+        int currentLetterSnake = 1;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
+                currentLetterSnake++;
+            } else {
+                if (currentLetterSnake > largest) {
+                    largest = currentLetterSnake;
+                }
+                currentLetterSnake = 1;
+            }
+        }
+
+        if (currentLetterSnake > largest) {
+            largest = currentLetterSnake;
+        }
+        return largest;
+    }
 
 
 
