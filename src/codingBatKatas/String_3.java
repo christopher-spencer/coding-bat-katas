4d63ec0309994b8da248dc0c1c26d30821ea0d1e
thebellywolf <cdspenzer@gmail.com>
2023-10-13 20:59:09 -0400

    same

--- src/codingBatKatas/String_3.java
+++ src/codingBatKatas/String_3.java
@@ -160,6 +160,14 @@ public int countYZ(String str) {
         return sum;
     }
 
+//    Given a string, return the longest substring that appears at both the beginning and end of the string without
+//    overlapping. For example, sameEnds("abXab") is "ab".
+//    sameEnds("abXYab") → "ab"
+//    sameEnds("xx") → "x"
+//    sameEnds("xxx") → "x"
+
+    
+
 
 
 
