a48b5660b3c6ca3b3dab45587e4af32a4427257a
thebellywolf <cdspenzer@gmail.com>
2023-10-16 21:18:32 -0400

    notReplace

--- src/codingBatKatas/String_3.java
+++ src/codingBatKatas/String_3.java
@@ -261,15 +261,33 @@ public int countYZ(String str) {
         if (!numString.isEmpty()) {
             sum += Integer.parseInt(numString);
         }
-        
+
         return sum;
     }
 
+//    Given a string, return a string where every appearance of the lowercase word "is" has been replaced with "is not".
+//    The word "is" should not be immediately preceeded or followed by a letter -- so for example the "is" in "this" does
+//    not count. (Note: Character.isLetter(char) tests if a char is a letter.)
+//    notReplace("is test") → "is not test"
+//    notReplace("is-is") → "is not-is not"
+//    notReplace("This is right") → "This is not right"
 
+    public String notReplace(String str) {
+        String newString = "";
 
-
-
-
-
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 1 < str.length() && str.substring(i, i + 2).equals("is")) {
+                if ((i == 0 || !Character.isLetter(str.charAt(i - 1))) && (i + 2 == str.length() || !Character.isLetter(str.charAt(i + 2)))) {
+                    newString += "is not";
+                    i++;
+                } else {
+                    newString += str.charAt(i);
+                }
+            } else {
+                newString += str.charAt(i);
+            }
+        }
+        return newString;
+    }
 
 }
