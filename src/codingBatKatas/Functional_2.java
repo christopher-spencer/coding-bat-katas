1bfbff251c6d71326813ab3c82a34ca8ba3d0538
thebellywolf <cdspenzer@gmail.com>
2023-10-18 17:39:46 -0400

    noLong

--- src/codingBatKatas/Functional_2.java
+++ src/codingBatKatas/Functional_2.java
@@ -46,6 +46,15 @@ public class Functional_2 {
         return strings;
     }
 
+//    Given a list of strings, return a list of the strings, omitting any string length 4 or more.
+//    noLong(["this", "not", "too", "long"]) → ["not", "too"]
+//    noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+//    noLong(["cccc", "cccc", "cccc"]) → []
+
+    public List<String> noLong(List<String> strings) {
+        strings.removeIf(s -> s.length() >= 4);
+        return strings;
+    }
 
 
 
