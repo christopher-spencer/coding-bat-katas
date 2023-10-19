79dd14a36c04f559c057243bb18574fe8519c13d
thebellywolf <cdspenzer@gmail.com>
2023-10-19 14:00:31 -0400

    bigDiff

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -18,6 +18,26 @@ public class Array_2 {
         return counter;
     }
 
+//    Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array.
+//        Note: the built-in Math.min(v1, v2) and Math.max(v1, v2) methods return the smaller or larger of two values.
+//    bigDiff([10, 3, 5, 6]) → 7
+//    bigDiff([7, 2, 10, 9]) → 8
+//    bigDiff([2, 10, 7, 2]) → 8
+
+    public int bigDiff(int[] nums) {
+        int largest = nums[0];
+        int smallest = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (largest < nums[i] ) {
+                largest = nums[i];
+            }
+            if (smallest > nums[i]) {
+                smallest = nums[i];
+            }
+        }
+        return largest - smallest;
+    }
 
 
 
