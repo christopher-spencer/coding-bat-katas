0566215f697db7d8c0d95ca01a8a05f2b8a46524
thebellywolf <cdspenzer@gmail.com>
2023-10-20 17:55:54 -0400

    sum67

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -92,6 +92,29 @@ public class Array_2 {
         return sum;
     }
 
+//    Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the
+//        next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+//    sum67([1, 2, 2]) → 5
+//    sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+//    sum67([1, 1, 6, 7, 2]) → 4
+
+    public int sum67(int[] nums) {
+        int sum = 0;
+        boolean between6and7 = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 6) {
+                between6and7 = true;
+            } else if (between6and7 && nums[i] == 7) {
+                between6and7 = false;
+            } else if (!between6and7) {
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+
+
 
 
 
