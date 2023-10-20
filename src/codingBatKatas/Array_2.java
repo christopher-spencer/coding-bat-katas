df34b4d033fa6ee651b16f46ea261c25d33a1b30
thebellywolf <cdspenzer@gmail.com>
2023-10-20 18:01:29 -0400

    lucy13

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -129,6 +129,23 @@ public class Array_2 {
         return false;
     }
 
+//    Given an array of ints, return true if the array contains no 1's and no 3's.
+//    lucky13([0, 2, 4]) → true
+//    lucky13([1, 2, 3]) → false
+//    lucky13([1, 2, 4]) → false
+
+    public boolean lucky13(int[] nums) {
+        boolean isNo13 = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1 || nums[i] == 3) {
+                isNo13 = false;
+            }
+        }
+        return isNo13;
+    }
+
+
 
 
 
