2fb042de6cf9322809d1234382f95df319f86ae8
thebellywolf <cdspenzer@gmail.com>
2023-10-20 18:51:53 -0400

    no14

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -235,6 +235,26 @@ public class Array_2 {
         return newString;
     }
 
+//    Given an array of ints, return true if it contains no 1's or it contains no 4's.
+//    no14([1, 2, 3]) → true
+//    no14([1, 2, 3, 4]) → false
+//    no14([2, 3, 4]) → true
+
+    public boolean no14(int[] nums) {
+        boolean no1s = true;
+        boolean no4s = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                no1s = false;
+            }
+            if (nums[i] == 4) {
+                no4s = false;
+            }
+        }
+        return no1s || no4s;
+    }
+
+
 
 
 
