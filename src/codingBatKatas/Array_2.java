57947ed041d12dd091e17442c9b4ca13af0a497c
thebellywolf <cdspenzer@gmail.com>
2023-10-20 18:22:21 -0400

    only14

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -200,6 +200,22 @@ public class Array_2 {
         return nLength;
     }
 
+//    Given an array of ints, return true if every element is a 1 or a 4.
+//    only14([1, 4, 1, 4]) → true
+//    only14([1, 4, 2, 4]) → false
+//    only14([1, 1]) → true
+
+    public boolean only14(int[] nums) {
+        boolean all41 = true;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 1 && nums[i] != 4) {
+                all41 = false;
+                break;
+            }
+        }
+        return all41;
+    }
 
 
 
