978e791b2389049d75165521de5819931fe091c5
thebellywolf <cdspenzer@gmail.com>
2023-10-20 18:09:33 -0400

    more14

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -161,6 +161,25 @@ public class Array_2 {
         return counterOf2s == 8;
     }
 
+//    Given an array of ints, return true if the number of 1's is greater than the number of 4's
+//    more14([1, 4, 1]) → true
+//    more14([1, 4, 1, 4]) → false
+//    more14([1, 1]) → true
+
+    public boolean more14(int[] nums) {
+        int count4s = 0;
+        int count1s = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 4) {
+                count4s++;
+            }
+            if (nums[i] == 1) {
+                count1s++;
+            }
+        }
+        return count4s < count1s;
+    }
 
 
 
