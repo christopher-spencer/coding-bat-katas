a50b7f8a791c8f39d78f6f5118367c236f103970
thebellywolf <cdspenzer@gmail.com>
2023-10-20 18:05:17 -0400

    sum28

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -145,6 +145,23 @@ public class Array_2 {
         return isNo13;
     }
 
+//    Given an array of ints, return true if the sum of all the 2's in the array is exactly 8.
+//    sum28([2, 3, 2, 2, 4, 2]) → true
+//    sum28([2, 3, 2, 2, 4, 2, 2]) → false
+//    sum28([1, 2, 3, 4]) → false
+
+    public boolean sum28(int[] nums) {
+        int counterOf2s = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 2) {
+                counterOf2s+= 2;
+            }
+        }
+        return counterOf2s == 8;
+    }
+
+
 
 
 
