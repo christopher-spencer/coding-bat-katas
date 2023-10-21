b4392070420f7628ad6a70c325e4a1e9f93c47bd
thebellywolf <cdspenzer@gmail.com>
2023-10-21 09:49:28 -0400

    has77

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -315,6 +315,22 @@ public class Array_2 {
         return counter;
     }
 
+//    Given an array of ints, return true if the array contains two 7's next to each other, or there are two 7's separated
+//    by one element, such as with {7, 1, 7}.
+//    has77([1, 7, 7]) → true
+//    has77([1, 7, 1, 7]) → true
+//    has77([1, 7, 1, 1, 7]) → false
+
+    public boolean has77(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            if ((nums[i-1] == 7 && nums[i] == 7) || (i+1 < nums.length && nums[i-1] == 7 && nums[i+1] == 7)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 
 
 
