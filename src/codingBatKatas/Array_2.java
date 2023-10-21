c7a8a7ea0580bc9f5159e7fbcafd11b43fa8c370
thebellywolf <cdspenzer@gmail.com>
2023-10-21 09:33:09 -0400

    either24

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -271,7 +271,29 @@ public class Array_2 {
         return isEverywhere;
     }
 
+//    Given an array of ints, return true if the array contains a 2 next to a 2 or a 4 next to a 4, but not both.
+//    either24([1, 2, 2]) → true
+//    either24([4, 4, 1]) → true
+//    either24([4, 4, 1, 2, 2]) → false
 
+    public boolean either24(int[] nums) {
+        boolean twoTwo = false;
+        boolean fourFour = false;
+
+        if (nums.length < 2) {
+            return false;
+        }
+
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == 2 && nums[i+1] == 2) {
+                twoTwo = true;
+            }
+            if (nums[i] == 4 && nums[i+1] == 4) {
+                fourFour = true;
+            }
+        }
+        return (twoTwo && !fourFour) || (fourFour && !twoTwo);
+    }
 
 
 
