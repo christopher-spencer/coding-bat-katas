b69d059f49779018d5d1ef74686b44c4ff93dbc6
thebellywolf <cdspenzer@gmail.com>
2023-10-21 10:16:04 -0400

    haveThree

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -368,6 +368,30 @@ public class Array_2 {
         return false;
     }
 
+//    Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.
+//    haveThree([3, 1, 3, 1, 3]) → true
+//    haveThree([3, 1, 3, 3]) → false
+//    haveThree([3, 4, 3, 3, 4]) → false
+
+    public boolean haveThree(int[] nums) {
+        int threeCounter = 0;
+
+        if (nums.length > 0 && nums[0] == 3) {
+            threeCounter++;
+        }
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == 3) {
+                if (nums[i-1] != 3) {
+                    threeCounter++;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return threeCounter == 3;
+    }
+
 
 
 
