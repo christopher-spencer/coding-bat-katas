4212ecad71f1678dfbc180eb4d054bbdf2570a56
thebellywolf <cdspenzer@gmail.com>
2023-10-21 09:52:26 -0400

    has12

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -330,6 +330,27 @@ public class Array_2 {
         return false;
     }
 
+//    Given an array of ints, return true if there is a 1 in the array with a 2 somewhere later in the array.
+//    has12([1, 3, 2]) → true
+//    has12([3, 1, 2]) → true
+//    has12([3, 1, 4, 5, 2]) → true
+
+    public boolean has12(int[] nums) {
+        boolean isOne = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                isOne = true;
+            }
+            if (isOne) {
+                if (nums[i] == 2) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
 
 
