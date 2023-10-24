d7e41d61e5a54bef5b9709b4124aaab684057592
thebellywolf <cdspenzer@gmail.com>
2023-10-23 21:02:24 -0400

    tenRun

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -496,6 +496,39 @@ public class Array_2 {
         return numsArray;
     }
 
+//    For each multiple of 10 in the given array, change all the values following it to be that multiple of 10, until
+//    encountering another multiple of 10. So {2, 10, 3, 4, 20, 5} yields {2, 10, 10, 10, 20, 20}.
+//    tenRun([2, 10, 3, 4, 20, 5]) → [2, 10, 10, 10, 20, 20]
+//    tenRun([10, 1, 20, 2]) → [10, 10, 20, 20]
+//    tenRun([10, 1, 9, 20]) → [10, 10, 10, 20]
+
+    public int[] tenRun(int[] nums) {
+        int[] tenArray = new int[nums.length];
+        int tenMultiple = 0;
+
+        if (nums.length < 2) {
+            return nums;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] % 10 == 0) {
+                tenArray[i] = nums[i];
+                tenMultiple = nums[i];
+            }
+
+            if (tenMultiple != 0) {
+                tenArray[i] = tenMultiple;
+            } else {
+                tenArray[i] = nums[i];
+            }
+
+            if (nums[0] == 0) {
+                tenArray[i] = 0;
+            }
+        }
+        return tenArray;
+    }
 
 
 
