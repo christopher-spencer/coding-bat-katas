c42ab2f737d21639951f8379bef1cdc5e41c1571
thebellywolf <cdspenzer@gmail.com>
2023-10-23 20:53:23 -0400

    shiftLeft

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -476,6 +476,26 @@ public class Array_2 {
         return newArray;
     }
 
+//    Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}. You may modify and return the given array, or return a new array.
+//    shiftLeft([6, 2, 5, 3]) → [2, 5, 3, 6]
+//    shiftLeft([1, 2]) → [2, 1]
+//    shiftLeft([1]) → [1]
+
+    public int[] shiftLeft(int[] nums) {
+        int[] numsArray = new int[nums.length];
+
+        if (nums.length < 2) {
+            return nums;
+        }
+
+        numsArray[nums.length-1] = nums[0];
+
+        for (int i = 0; i < nums.length-1; i++) {
+            numsArray[i] = nums[i+1];
+        }
+        return numsArray;
+    }
+
 
 
 
