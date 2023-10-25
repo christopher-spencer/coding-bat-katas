9c63ef4f20e639c0a687f873bed9ac9a99c3d9bd
thebellywolf <cdspenzer@gmail.com>
2023-10-25 16:08:27 -0400

    withoutTen

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -645,6 +645,39 @@ public class Array_2 {
         return newArray;
     }
 
+//    Return a version of the given array where all the 10's have been removed. The remaining elements should shift left
+//    towards the start of the array as needed, and the empty spaces a the end of the array should be 0. So {1, 10, 10, 2}
+//    yields {1, 2, 0, 0}. You may modify and return the given array or make a new array.
+//    withoutTen([1, 10, 10, 2]) → [1, 2, 0, 0]
+//    withoutTen([10, 2, 10]) → [2, 0, 0]
+//    withoutTen([1, 99, 10]) → [1, 99, 0]
+
+    public int[] withoutTen(int[] nums) {
+        int tenCounter = 0;
+        int[] newArray = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 10) {
+                tenCounter++;
+            }
+
+            if (nums[i] != 10) {
+                newArray[i- tenCounter] = nums[i];
+            }
+        }
+
+        for (int j = tenCounter; j > 0; j--) {
+            newArray[nums.length-j] = 0;
+        }
+        return newArray;
+    }
+
+
+
+
+
+
+
 
 
 }
