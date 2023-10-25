645781bc2c5358b07f0c182dc651d744e99057b4
thebellywolf <cdspenzer@gmail.com>
2023-10-25 16:09:15 -0400

    zeroMax

--- src/codingBatKatas/Array_2.java
+++ src/codingBatKatas/Array_2.java
@@ -672,6 +672,30 @@ public class Array_2 {
         return newArray;
     }
 
+//    Return a version of the given array where each zero value in the array is replaced by the largest odd value to the
+//    right of the zero in the array. If there is no odd value to the right of the zero, leave the zero as a zero.
+//    zeroMax([0, 5, 0, 3]) → [5, 5, 3, 3]
+//    zeroMax([0, 4, 0, 3]) → [3, 4, 3, 3]
+//    zeroMax([0, 1, 0]) → [1, 1, 0]
+
+    public int[] zeroMax(int[] nums) {
+
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i] == 0) {
+                int zeroIndex = i;
+                int largestOdd = 0;
+
+                for (int j = zeroIndex; j < nums.length; j++) {
+                    if (nums[j] > largestOdd && nums[j] % 2 != 0) {
+                        largestOdd = nums[j];
+                        nums[zeroIndex] = largestOdd;
+                    }
+                }
+            }
+        }
+        return nums;
+    }
+
 
 
 
