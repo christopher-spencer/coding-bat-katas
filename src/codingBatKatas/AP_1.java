7b469bda89560f1947439603bf1a47806d480d10
thebellywolf <cdspenzer@gmail.com>
2023-10-28 14:47:10 -0400

    hasOne

--- src/codingBatKatas/AP_1.java
+++ src/codingBatKatas/AP_1.java
@@ -130,6 +130,16 @@ public class AP_1 {
         return newStrings;
     }
 
+//    Given a positive int n, return true if it contains a 1 digit. Note: use % to get the rightmost digit, and / to discard the rightmost digit.
+//    hasOne(10) → true
+//    hasOne(22) → false
+//    hasOne(220) → false
+
+    public boolean hasOne(int n) {
+        String numberStr = Integer.toString(n);
+        return numberStr.contains("1");
+    }
+
 
 
 
