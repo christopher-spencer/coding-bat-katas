1a53eed6c63f46ccf217cdaae040390cc104681c
thebellywolf <cdspenzer@gmail.com>
2023-10-28 14:10:21 -0400

    wordsCount

--- src/codingBatKatas/AP_1.java
+++ src/codingBatKatas/AP_1.java
@@ -80,6 +80,22 @@ public class AP_1 {
         return average;
     }
 
+//    Given an array of strings, return the count of the number of strings with the given length.
+//    wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+//    wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+//    wordsCount(["a", "bb", "b", "ccc"], 4) → 0
+
+    public int wordsCount(String[] words, int len) {
+        int counter = 0;
+
+        for (String word : words) {
+            if (word.length() == len) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
 
 
 
