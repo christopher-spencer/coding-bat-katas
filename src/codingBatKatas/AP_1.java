e6c923b5cb3477c6cbe40e20116da0312ffd270f
thebellywolf <cdspenzer@gmail.com>
2023-10-28 14:06:50 -0400

    scoresAverage

--- src/codingBatKatas/AP_1.java
+++ src/codingBatKatas/AP_1.java
@@ -48,6 +48,37 @@ public class AP_1 {
         return false;
     }
 
+//    Given an array of scores, compute the int average of the first half and the second half, and return whichever is larger.
+//    We'll say that the second half begins at index length/2. The array length will be at least 2. To practice decomposition,
+//    write a separate helper method int average(int[] scores, int start, int end) { which computes the average of the elements
+//    between indexes start..end. Call your helper method twice to implement scoresAverage(). Write your helper method after
+//    your scoresAverage() method in the JavaBat text area. Normally you would compute averages with doubles, but here we use
+//    ints so the expected results are exact.
+//    scoresAverage([2, 2, 4, 4]) → 4
+//    scoresAverage([4, 4, 4, 2, 2, 2]) → 4
+//    scoresAverage([3, 4, 5, 1, 2, 3]) → 4
+
+    public int scoresAverage(int[] scores) {
+        int secondHalf = scores.length / 2;
+        int beginAverage = average(scores, 0, secondHalf - 1);
+        int endAverage = average(scores, secondHalf, scores.length - 1);
+        int largestAverage = Math.max(beginAverage, endAverage);
+
+        return largestAverage;
+    }
+
+    public int average(int[] scores, int start, int end) {
+        int totalIndexes = end - start + 1;
+        int sum = 0;
+        int average = 0;
+
+        for (int i = start; i <= end; i++) {
+            sum += scores[i];
+        }
+
+        average = sum / totalIndexes;
+        return average;
+    }
 
 
 
