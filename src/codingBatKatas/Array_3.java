fbc31c2c85ea35f932fa287f78cc24fa3640517f
thebellywolf <cdspenzer@gmail.com>
2023-10-28 12:55:52 -0400

    countClumps

--- src/codingBatKatas/Array_3.java
+++ src/codingBatKatas/Array_3.java
@@ -209,6 +209,31 @@ public class Array_3 {
         return maxMirrorLength;
     }
 
+//    Say that a "clump" in an array is a series of 2 or more adjacent elements of the same value. Return the number of clumps in the given array.
+//    countClumps([1, 2, 2, 3, 4, 4]) → 2
+//    countClumps([1, 1, 2, 1, 1]) → 2
+//    countClumps([1, 1, 1, 1, 1]) → 1
+
+    public int countClumps(int[] nums) {
+        int clumps = 0;
+
+        for (int i = 0; i < nums.length-1; i++) {
+
+            if (nums[i] == nums[i+1]) {
+                int sameNumCounter = 1;
+                while (i < nums.length-1 && nums[i] == nums[i+1]) {
+                    sameNumCounter++;
+                    i++;
+                }
+                if (sameNumCounter >= 2) {
+                    clumps++;
+                }
+            }
+        }
+        return clumps;
+    }
+
+
 
 
 
