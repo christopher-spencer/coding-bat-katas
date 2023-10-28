00e74740556570490c2fa38f071e835065d20005
thebellywolf <cdspenzer@gmail.com>
2023-10-28 12:55:12 -0400

    maxMirror

--- src/codingBatKatas/Array_3.java
+++ src/codingBatKatas/Array_3.java
@@ -181,6 +181,35 @@ public class Array_3 {
         return newArray;
     }
 
+//    We'll say that a "mirror" section in an array is a group of contiguous elements such that somewhere in the array,
+//    the same group appears in reverse order. For example, the largest mirror section in {1, 2, 3, 8, 9, 3, 2, 1} is length
+//        3 (the {1, 2, 3} part). Return the size of the largest mirror section found in the given array.
+//    maxMirror([1, 2, 3, 8, 9, 3, 2, 1]) → 3
+//    maxMirror([1, 2, 1, 4]) → 3
+//    maxMirror([7, 1, 2, 9, 7, 2, 1]) → 2
+
+    public int maxMirror(int[] nums) {
+        int maxMirrorLength = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j >= 0; j--) {
+                int mirrorMirror = i;
+                int onTheWall = j;
+                int currentMirrorLength = 0;
+
+                while (mirrorMirror < nums.length && onTheWall >= 0
+                        && nums[mirrorMirror] == nums[onTheWall]) {
+                    currentMirrorLength++;
+                    mirrorMirror++;
+                    onTheWall--;
+                }
+                maxMirrorLength = Math.max(maxMirrorLength, currentMirrorLength);
+            }
+        }
+        return maxMirrorLength;
+    }
+
+
 
 
 
