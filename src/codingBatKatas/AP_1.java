f345a56f5a81eb5d9613786817281958672cf364
thebellywolf <cdspenzer@gmail.com>
2023-10-29 14:00:01 -0400

    copyEndy

--- src/codingBatKatas/AP_1.java
+++ src/codingBatKatas/AP_1.java
@@ -185,6 +185,38 @@ public class AP_1 {
         return newArray;
     }
 
+//    We'll say that a positive int n is "endy" if it is in the range 0..10 or 90..100 (inclusive). Given an array of
+//    positive ints, return a new array of length "count" containing the first endy numbers from the original array.
+//    Decompose out a separate isEndy(int n) method to test if a number is endy. The original array will contain at least
+//    "count" endy numbers.
+//    copyEndy([9, 11, 90, 22, 6], 2) → [9, 90]
+//    copyEndy([9, 11, 90, 22, 6], 3) → [9, 90, 6]
+//    copyEndy([12, 1, 1, 13, 0, 20], 2) → [1, 1]
+
+    public int[] copyEndy(int[] nums, int count) {
+        int[] newArray = new int[count];
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            while (counter < newArray.length) {
+                if (isEndy(nums[i])) {
+                    newArray[counter] = nums[i];
+                    counter++;
+                }
+                break;
+            }
+        }
+        return newArray;
+    }
+
+    public boolean isEndy(int n) {
+        if ( (n >= 0 && n <= 10) || (n >= 90 && n <= 100) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 
 
