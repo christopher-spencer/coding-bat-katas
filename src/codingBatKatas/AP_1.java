b83b14019bd1de4041c561a2407f7bbc42f8f7b9
thebellywolf <cdspenzer@gmail.com>
2023-10-29 14:02:16 -0400

    wordsWithout

--- src/codingBatKatas/AP_1.java
+++ src/codingBatKatas/AP_1.java
@@ -260,7 +260,36 @@ public class AP_1 {
         return score;
     }
 
+//    Given an array of strings, return a new array without the strings that are equal to the target string. One approach
+//    is to count the occurrences of the target string, make a new array of the correct length, and then copy over the
+//    correct strings.
+//    wordsWithout(["a", "b", "c", "a"], "a") → ["b", "c"]
+//    wordsWithout(["a", "b", "c", "a"], "b") → ["a", "c", "a"]
+//    wordsWithout(["a", "b", "c", "a"], "c") → ["a", "b", "a"]
 
+    public String[] wordsWithout(String[] words, String target) {
+        int targetCounter = 0;
+        int notTargetIndex = 0;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i] == target) {
+                targetCounter++;
+            }
+        }
+
+        String[] newArray = new String[words.length-targetCounter];
+
+        for (int j = 0; j < words.length; j++) {
+            while (notTargetIndex < newArray.length) {
+                if (words[j] != target) {
+                    newArray[notTargetIndex] = words[j];
+                    notTargetIndex++;
+                }
+                break;
+            }
+        }
+        return newArray;
+    }
 
 
 
