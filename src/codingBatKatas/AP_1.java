cf4084d332b883c0a8a5ea4a65eaa3e8ce0f4b38
thebellywolf <cdspenzer@gmail.com>
2023-10-30 15:20:56 -0400

    commonTwo

--- src/codingBatKatas/AP_1.java
+++ src/codingBatKatas/AP_1.java
@@ -440,6 +440,44 @@ public class AP_1 {
         return newString;
     }
 
+//    Start with two arrays of strings, a and b, each in alphabetical order, possibly with duplicates. Return the count of
+//    the number of strings which appear in both arrays. The best "linear" solution makes a single pass over both arrays,
+//    taking advantage of the fact that they are in alphabetical order.
+//    commonTwo(["a", "c", "x"], ["b", "c", "d", "x"]) → 2
+//    commonTwo(["a", "c", "x"], ["a", "b", "c", "x", "z"]) → 3
+//    commonTwo(["a", "b", "c"], ["a", "b", "c"]) → 3
+
+    public int commonTwo(String[] a, String[] b) {
+        int counter = 0;
+        int i = 0;
+        int j = 0;
+
+        while (i < a.length && j < b.length) {
+            int compareStrings = a[i].compareTo(b[j]);
+
+            if (compareStrings < 0) {
+                i++;
+            } else if (compareStrings > 0) {
+                j++;
+            } else {
+                counter++;
+                i++;
+                j++;
+
+                while (i < a.length && a[i].equals(a[i - 1])) {
+                    i++;
+                }
+
+                while (j < b.length && b[j].equals(b[j - 1])) {
+                    j++;
+                }
+            }
+        }
+
+        return counter;
+    }
+
+
 
 
 
