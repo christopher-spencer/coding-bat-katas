bee9d756e05f10af55e25b7d516ceb04e46009e7
thebellywolf <cdspenzer@gmail.com>
2023-10-31 15:08:02 -0400

    bunnyEars

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -16,6 +16,18 @@ public class Recursion_1 {
         return n * factorial(n-1);
     }
 
+//    We have a number of bunnies and each bunny has two big floppy ears. We want to compute the total number of ears across
+//    all the bunnies recursively (without loops or multiplication).
+//    bunnyEars(0) → 0
+//    bunnyEars(1) → 2
+//    bunnyEars(2) → 4
+
+    public int bunnyEars(int bunnies) {
+        if (bunnies == 0) {
+            return 0;
+        }
+        return 2 + bunnyEars(bunnies - 1);
+    }
 
 
 
