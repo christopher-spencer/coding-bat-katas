f3bc299668c2c88915b4d3797a8e77f2e4660496
thebellywolf <cdspenzer@gmail.com>
2023-10-31 16:01:33 -0400

    allStar

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -305,6 +305,22 @@ public class Recursion_1 {
         }
     }
 
+//    Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+//    allStar("hello") → "h*e*l*l*o"
+//    allStar("abc") → "a*b*c"
+//    allStar("ab") → "a*b"
+
+    public String allStar(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+
+        return str.charAt(0) + "*" + allStar(str.substring(1));
+    }
+
+
+
+
 
 
 
