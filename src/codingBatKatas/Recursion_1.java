26c2596e358d11736cf4759b844994e436387465
thebellywolf <cdspenzer@gmail.com>
2023-10-31 15:14:04 -0400

    countX

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -162,6 +162,24 @@ public class Recursion_1 {
         return base * powerN(base, n - 1);
     }
 
+//    Given a string, compute recursively (no loops) the number of lowercase 'x' chars in the string.
+//    countX("xxhixx") → 4
+//    countX("xhixhix") → 3
+//    countX("hi") → 0
+
+    public int countX(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        if (str.charAt(0) == 'x') {
+            return 1 + countX(str.substring(1));
+        } else {
+            return countX(str.substring(1));
+        }
+    }
+
+
 
 
 
