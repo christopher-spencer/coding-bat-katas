4f77f30d588993433d99f57eb32a5410912c4af5
thebellywolf <cdspenzer@gmail.com>
2023-10-31 15:25:41 -0400

    changePi

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -214,6 +214,22 @@ public class Recursion_1 {
         }
     }
 
+//    Given a string, compute recursively (no loops) a new string where all appearances of "pi" have been replaced by "3.14".
+//    changePi("xpix") → "x3.14x"
+//    changePi("pipi") → "3.143.14"
+//    changePi("pip") → "3.14p"
+
+    public String changePi(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+
+        if (str.contains("pi") && str.charAt(0) == 'p' && str.charAt(1) == 'i' ) {
+            return "3.14" + changePi(str.substring(2));
+        } else {
+            return str.charAt(0) + changePi(str.substring(1));
+        }
+    }
 
 
 
