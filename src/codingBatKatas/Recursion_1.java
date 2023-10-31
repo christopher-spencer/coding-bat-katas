33b43ff22cd580fe69e590d2438aa34f39f03495
thebellywolf <cdspenzer@gmail.com>
2023-10-31 15:14:43 -0400

    countHi

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -179,6 +179,24 @@ public class Recursion_1 {
         }
     }
 
+//    Given a string, compute recursively (no loops) the number of times lowercase "hi" appears in the string.
+//    countHi("xxhixx") → 1
+//    countHi("xhixhix") → 2
+//    countHi("hi") → 1
+
+    public int countHi(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+
+        if (str.contains("hi") && str.charAt(0) == 'h' && str.charAt(1) == 'i') {
+            return 1 + countHi(str.substring(1));
+        } else {
+            return countHi(str.substring(1));
+        }
+    }
+
+
 
 
 
