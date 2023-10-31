48165179a8eb7344188ba18f9cc3740356188d9a
thebellywolf <cdspenzer@gmail.com>
2023-10-31 15:11:48 -0400

    count7

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -95,6 +95,32 @@ public class Recursion_1 {
         return rightmostDigit + sumDigits(remainingDigits);
     }
 
+//    Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops).
+//    Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit
+//    (126 / 10 is 12).
+//    count7(717) → 2
+//    count7(7) → 1
+//    count7(123) → 0
+
+    public int count7(int n) {
+        if (n < 10) {
+            if (n == 7) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        int rightmostDigit = n % 10;
+        int remainingDigits = n / 10;
+
+        if (rightmostDigit == 7) {
+            return 1 + count7(remainingDigits);
+        } else {
+            return count7(remainingDigits);
+        }
+    }
+
 
 
 
