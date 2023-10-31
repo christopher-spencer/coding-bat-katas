fbf58e45dbc081695095cbcebd03788110c18123
thebellywolf <cdspenzer@gmail.com>
2023-10-31 15:11:02 -0400

    sumDigits

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -79,6 +79,21 @@ public class Recursion_1 {
         return rows + triangle(rows-1);
     }
 
+//    Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the
+//    rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+//    sumDigits(126) → 9
+//    sumDigits(49) → 13
+//    sumDigits(12) → 3
+
+    public int sumDigits(int n) {
+        if (n < 10) {
+            return n;
+        }
+        int rightmostDigit = n % 10;
+        int remainingDigits = n / 10;
+
+        return rightmostDigit + sumDigits(remainingDigits);
+    }
 
 
 
