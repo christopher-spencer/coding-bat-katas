0d1081e1cc2c93e03ec44b5ded3b90de280d7cc6
thebellywolf <cdspenzer@gmail.com>
2023-11-01 13:07:00 -0400

    parentBit

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -446,6 +446,25 @@ public class Recursion_1 {
         }
     }
 
+//    Given a string that contains a single pair of parenthesis, compute recursively a new string made of only of the
+//    parenthesis and their contents, so "xyz(abc)123" yields "(abc)".
+//    parenBit("xyz(abc)123") → "(abc)"
+//    parenBit("x(hello)") → "(hello)"
+//    parenBit("(xy)1") → "(xy)"
+
+    public String parenBit(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+
+        if (str.charAt(0) == '(' ) {
+            int endIndex = str.indexOf( ')' );
+
+            return str.substring(0, endIndex + 1);
+        }
+        return parenBit(str.substring(1));
+    }
+
 
 
 
