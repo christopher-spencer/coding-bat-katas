f9b6a88a69dfad2d32185ba1bbd551872776a8ff
thebellywolf <cdspenzer@gmail.com>
2023-11-01 13:07:43 -0400

    nestParen

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -465,6 +465,25 @@ public class Recursion_1 {
         return parenBit(str.substring(1));
     }
 
+//    Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))".
+//    Suggestion: check the first and last chars, and then recur on what's inside them.
+//    nestParen("(())") → true
+//    nestParen("((()))") → true
+//    nestParen("(((x))") → false
+
+    public boolean nestParen(String str) {
+        if (str.isEmpty()) {
+            return true;
+        }
+
+        if (str.charAt(0) != '(' && str.charAt(str.length()-1) != ')' ) {
+            return false;
+        } else if (str.charAt(0) == '(' && str.charAt(str.length()-1) == ')' ) {
+            return nestParen(str.substring(1,str.length()-1 ) );
+        }
+        return false;
+    }
+
 
 
 
