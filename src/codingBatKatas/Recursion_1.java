bdd3614011eff772c42a2dfa897c462813d50d8b
thebellywolf <cdspenzer@gmail.com>
2023-11-01 13:03:03 -0400

    endX

--- src/codingBatKatas/Recursion_1.java
+++ src/codingBatKatas/Recursion_1.java
@@ -337,6 +337,22 @@ public class Recursion_1 {
     }
 
 
+//    Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string.
+//    endX("xxre") → "rexx"
+//    endX("xxhixx") → "hixxxx"
+//    endX("xhixhix") → "hihixxx"
+
+    public String endX(String str) {
+        if (str.length() < 2) {
+            return str;
+        }
+
+        if (str.charAt(0) == 'x') {
+            return endX(str.substring(1)) + str.charAt(0);
+        } else {
+            return str.charAt(0) + endX(str.substring(1));
+        }
+    }
 
 
 
