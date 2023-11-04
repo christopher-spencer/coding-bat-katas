a596516d206956ce814a7c8e4a75e91040065e6a
thebellywolf <cdspenzer@gmail.com>
2023-11-04 15:34:25 -0400

    splitOdd10

--- src/codingBatKatas/Recursion_2.java
+++ src/codingBatKatas/Recursion_2.java
@@ -173,7 +173,35 @@ public class Recursion_2 {
         return false;
     }
 
+//    Given an array of ints, is it possible to divide the ints into two groups, so that the sum of one group is a multiple
+//    of 10, and the sum of the other group is odd. Every int must be in one group or the other. Write a recursive helper
+//    method that takes whatever arguments you like, and make the initial call to your recursive helper from splitOdd10().
+//    (No loops needed.)
+//    splitOdd10([5, 5, 5]) → true
+//    splitOdd10([5, 5, 6]) → false
+//    splitOdd10([5, 5, 6, 1]) → true
 
+    public boolean splitOdd10(int[] nums) {
+        return splitHelper(0, nums, 0, 0);
+    }
+
+    private boolean splitHelper(int start, int[] nums, int sum1, int sum2) {
+        if (start >= nums.length) {
+            if (sum1 % 10 == 0 && sum2 % 2 != 0) {
+                return true;
+            }
+            return false;
+        }
+
+        if (splitHelper(start+1, nums, sum1 + nums[start], sum2)) {
+            return true;
+        }
+
+        if (splitHelper(start+1, nums, sum1, sum2 + nums[start])) {
+            return true;
+        }
+        return false;
+    }
 
 
 
