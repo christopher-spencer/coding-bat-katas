116ffbe96be1a045b357edf94d75ca8699533427
thebellywolf <cdspenzer@gmail.com>
2023-11-04 14:33:57 -0400

    groupSumClump

--- src/codingBatKatas/Recursion_2.java
+++ src/codingBatKatas/Recursion_2.java
@@ -107,6 +107,41 @@ public class Recursion_2 {
         }
     }
 
+//    Given an array of ints, is it possible to choose a group of some of the ints, such that the group sums to the given
+//    target, with this additional constraint: if there are numbers in the array that are adjacent and the identical value,
+//    they must either all be chosen, or none of them chosen. For example, with the array {1, 2, 2, 2, 5, 2}, either all
+//    three 2's in the middle must be chosen or not, all as a group. (one loop can be used to find the extent of the
+//    identical values).
+//    groupSumClump(0, [2, 4, 8], 10) → true
+//    groupSumClump(0, [1, 2, 4, 8, 1], 14) → true
+//    groupSumClump(0, [2, 4, 4, 8], 14) → false
+
+    public boolean groupSumClump(int start, int[] nums, int target) {
+        if (start >= nums.length) {
+            if (target == 0) {
+                return true;
+            }
+            return false;
+        }
+
+        int end = start + 1;
+
+        while (end < nums.length && nums[end] == nums[start]) {
+            end++;
+        }
+
+        int sum = (end - start) * nums[start];
+
+        if (groupSumClump(end, nums, target - sum)) {
+            return true;
+        }
+
+        if (groupSumClump(end, nums, target)) {
+            return true;
+        }
+
+        return false;
+    }
 
 
 
