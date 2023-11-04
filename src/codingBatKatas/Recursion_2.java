fbb3eec21fb2f202b45531ed711c5d4aa39eada0
thebellywolf <cdspenzer@gmail.com>
2023-11-04 15:33:21 -0400

    splitArray

--- src/codingBatKatas/Recursion_2.java
+++ src/codingBatKatas/Recursion_2.java
@@ -143,6 +143,35 @@ public class Recursion_2 {
         return false;
     }
 
+//    Given an array of ints, is it possible to divide the ints into two groups, so that the sums of the two groups are the
+//    same. Every int must be in one group or the other. Write a recursive helper method that takes whatever arguments you
+//    like, and make the initial call to your recursive helper from splitArray(). (No loops needed.)
+//    splitArray([2, 2]) → true
+//    splitArray([2, 3]) → false
+//    splitArray([5, 2, 3]) → true
+
+    public boolean splitArray(int[] nums) {
+        return splitArrayHelper(0, nums, 0, 0);
+    }
+
+    private boolean splitArrayHelper(int start, int[] nums, int sum1, int sum2) {
+        if (start >= nums.length) {
+            if (sum1 == sum2) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (splitArrayHelper(start + 1, nums, sum1 + nums[start], sum2)) {
+            return true;
+        }
+
+        if (splitArrayHelper(start + 1, nums, sum1, sum2 + nums[start])) {
+            return true;
+        }
+        return false;
+    }
 
 
 
